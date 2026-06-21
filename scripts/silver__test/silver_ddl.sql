e6b68dad70b893230a7212e3f79e2b9460333bb0
MufeedKcp <mufeedkcp1@gmail.com>
2026-06-21 14:20:13 +0000

    modified sales details table

--- scripts/silver__test/silver_ddl.sql
+++ scripts/silver__test/silver_ddl.sql
@@ -37,12 +37,12 @@ CREATE TABLE IF NOT EXISTS silver_test.sales_details(
     sls_ord_num VARCHAR(50),
     sls_prd_key VARCHAR(50),
     sls_cust_id INT,
-    sls_order_dt INT,
-    sls_ship_dt INT,
-    sls_due_dt INT,
+    sls_order_dt DATE,
+    sls_ship_dt DATE,
+    sls_due_dt DATE,
     sls_sales INT,
     sls_quantity INT,
-    sls_price INT,
+    sls_price FLOAT,
     dwh_create_date DATETIME DEFAULT NOW()
 );
 
