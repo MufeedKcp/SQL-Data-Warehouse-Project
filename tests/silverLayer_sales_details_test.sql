1c7562b8e0fa9da80b4b55011999e06d1e866eee
MufeedKcp <mufeedkcp1@gmail.com>
2026-06-27 07:26:25 +0000

    added one test to silver layer sales details

--- tests/silverLayer_sales_details_test.sql
+++ tests/silverLayer_sales_details_test.sql
@@ -14,8 +14,16 @@ OR sls_order_dt > sls_due_dt;
 
 
 -- NULL or Negative amount
-SELECT 
-	sls_ord_num,
-	sls_sales
+SELECT DISTINCT 
+    sls_sales,
+    sls_quantity,
+    sls_price 
 FROM silver_test.sales_details
-WHERE sls_sales < 0 OR sls_sales IS NULL;
+WHERE sls_sales != sls_quantity * sls_price
+   OR sls_sales IS NULL 
+   OR sls_quantity IS NULL 
+   OR sls_price IS NULL
+   OR sls_sales <= 0 
+   OR sls_quantity <= 0 
+   OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price;
