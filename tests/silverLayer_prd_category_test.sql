d1f036bf073f30abbb26b12b30eb081df9887280
MufeedKcp <mufeedkcp1@gmail.com>
2026-06-27 14:59:48 +0000

    added test to silver layer prd_category

--- tests/silverLayer_prd_category_test.sql
+++ tests/silverLayer_prd_category_test.sql
@@ -1,5 +1,16 @@
 -- Check for Unwanted Spaces
--- Expectation: No Results
+SELECT 
+	cat,
+    subcat,
+    maintenance
+FROM silver_test.prd_category
+WHERE cat != TRIM(cat) 
+OR subcat != TRIM(subcat)
+OR maintenance != TRIM(maintenance);
 
 
--- Data Standardization & Consistency
+
+-- Data Standardization & Consistency for maintenance
+SELECT 
+	DISTINCT maintenance
+FROM silver_test.prd_category;
